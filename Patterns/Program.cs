12f24c8d309c755da2310c0d2e2d17cd838ca361
Лукина Анна Викторовна <Anna.Lukina@norbit.ru>
2018-04-13 10:29:10 +0300

    create Singleton pattern

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -10,6 +10,16 @@ namespace Patterns
     {
         static void Main(string[] args)
         {
+            do
+                {
+                    Console.WriteLine(Singleton.GetRandomNumber());
+                    Console.WriteLine(Singleton.GetRandomNumber());
+                    Console.WriteLine(Singleton.GetRandomNumber());
+                    Console.WriteLine(Singleton.GetRandomNumber());
+                }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+
         }
     }
 }
---
+++ Patterns/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    public static class Singleton
+    {
+        private static Random _random;
+
+        public static int GetRandomNumber()
+        {
+            if (_random == null)
+            {
+                _random = new Random();
+            }
+
+            return _random.Next();
+        }
+    }
+}
